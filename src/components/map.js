d54756a15e78e77ad7a89af22df605bc572e1297
recflow <chjh9107@gmail.com>
2022-10-12 18:11:52 +0900

    map 띄우기

--- src/components/map.js
+++
@@ -1,7 +0,0 @@
-export default function map() {
-    return (
-      <div>
-        
-      </div>
-    );
-  }
---
+++ src/pages/MainPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MapComponent from '../components/MapComponent';
+
+export default function MainPage() {
+    return (
+        <MapComponent/>
+    )
+}
