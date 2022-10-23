2f1a02e01a53cfe6d7e4f5f215e2ca0772726653
recflow <chjh9107@gmail.com>
2022-10-23 17:55:33 +0900

    접속시 현재 위치를 중앙으로 띄움

--- src/components/Map/MapComponent.js
+++ src/components/Map/MapComponent.js
@@ -1,12 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
+import { faLongArrowAltDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function MapComponent() {
-    useEffect(() => {
-      const script = document.createElement("script");
-      script.innerHTML = `         
+  useEffect(() => {
+    const script = document.createElement("script");
+    if (navigator.geolocation) {
+      console.log("현 위치 작업 가능");
+      navigator.geolocation.getCurrentPosition((position) => {
+        let pos = position.coords;
+        script.innerHTML =
+          `         
           function initTmap() {
               var map = new Tmapv2.Map("TMapApp", {
-                  center: new Tmapv2.LatLng(37.566481622437934,126.98502302169841),
+                  center: new Tmapv2.LatLng(` +
+          pos.latitude +
+          `,` +
+          pos.longitude +
+          `),
                   width: "100%",
                   height: "100%",
                   zoom:15
@@ -15,21 +25,21 @@ export default function MapComponent() {
           
           initTmap();
      `;
-      script.type = "text/javascript";
-      script.async = "async";
-      document.head.appendChild(script);
-    }, []);
- 
+      });
+    }
+    script.type = "text/javascript";
+    script.async = "async";
+    document.head.appendChild(script);
+  }, []);
 
-    return (
-        <div
-          id="TMapApp"
-          style={{
-            height: "100%",
-            width: "100%",
-            position: "fixed",
-          }}
-        />
-      );
-
+  return (
+    <div
+      id="TMapApp"
+      style={{
+        height: "100%",
+        width: "100%",
+        position: "fixed",
+      }}
+    />
+  );
 }
