ba60e2b2bbd260d0fa0d5b1312c5a0d1e77716dd
mohash <moharin9@gmail.com>
2022-11-15 14:54:41 +0900

    map 현재위치 안뜨는 것 수정

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import MainPage from './pages/MainPage';
 import React, { useEffect, useState } from "react";
 import './App.css';
 
+const { Tmapv2 }=window
 function App() {
 
   const [map, setMap] = useState(null);
-  const lng=126.98502302169841;
-  const lat=37.566481622437934;
   let zoom = 10;
 
   if(window.screen.width >= 768) {
@@ -15,28 +14,33 @@ function App() {
   
   
   function createMap(lng, lat) { 
-    setMap(new window.Tmapv2.Map("TMapContainer", {
+    setMap(new Tmapv2.Map("TMapContainer", {
       center: new window.Tmapv2.LatLng(lat, lng),
       width: "100%",
       height: "100vh",
       zoom: zoom,
       https: true,
     }));
-   
+
   }
 
   useEffect(() => {
+    const lng=126.98502302169841;
+    const lat=37.566481622437934;
 
     if(navigator.geolocation){
       navigator.geolocation.getCurrentPosition((position) => {
-        lng=position.coords.longitude
-        lat=position.coords.latitude
+        console.log("true")
+        const lng=position.coords.longitude
+        const lat=position.coords.latitude
+        console.log(lng, lat)
+        createMap(lng, lat)
       });
     }
     else { 
       alert("현재 위치를 찾을 수 없습니다.")
+      createMap(lng, lat)
     }
-    createMap(lng, lat)
   }, []);
 
 
